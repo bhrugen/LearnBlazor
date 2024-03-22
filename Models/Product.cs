38ce109204551f70800f9a02c47fdfcc0792f3de
Bhrugen Patel <bhrugen1990@hotmail.com>
2024-03-22 08:41:01 -0500

    Section 3 - One way data binding

---
+++ Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace LearnBlazor.Models
+{
+	public class Product
+	{
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
