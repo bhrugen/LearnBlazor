b5da24a9c9589a14aaad442f8aafc17128275264
Bhrugen Patel <bhrugen1990@gmail.com>
2024-05-01 16:06:58 -0500

    Section 7 - Toastr Part 1

---
+++ wwwroot/js/common.js
@@ -0,0 +1,8 @@
+﻿window.showToastr = function (type, message) {
+    if (type == "success") {
+        toastr.success(message);
+    }
+    if (type == "error") {
+        toastr.error(message);
+    }
+}  
