01d6d1faefd2d50776b04f0125b7ed45244df4b0
Bhrugen Patel <bhrugen1990@gmail.com>
2024-05-01 16:23:04 -0500

    Section 7 - JS Runtime Extensions

---
+++ Helper/IJSRuntimeExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.JSInterop;
+
+namespace LearnBlazor.Helper
+{
+    public static class IJSRuntimeExtensions
+    {
+
+        public static async Task ToastrSuccess(this IJSRuntime jsRuntime, string message)
+        {
+           await jsRuntime.InvokeVoidAsync("showToastr","success", message);
+        }
+
+        public static async Task ToastrError(this IJSRuntime jsRuntime, string message)
+        {
+            await jsRuntime.InvokeVoidAsync("showToastr", "error", message);
+        }
+
+    }
+}
