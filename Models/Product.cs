6bd012435c1ef8af3ce6361ab3e4918304be20a5
Bhrugen Patel <bhrugen1990@gmail.com>
2024-05-03 13:44:18 -0500

    Section 9 - Product Model Setup

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,11 +1,26 @@
-﻿namespace LearnBlazor.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LearnBlazor.Models
 {
 	public class Product
 	{
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Range(1,1000)]
         public double Price { get; set; }
         public bool IsActive { get; set; }
         public IEnumerable<Product_Prop> ProductProperties { get; set; }
+
+        public Category Category { get; set; }
+
+        public DateOnly AvailableAfter { get; set; }
+    }
+
+    public enum Category
+    {
+        Entree,
+        Appetizer,
+        Dessert
     }
 }
